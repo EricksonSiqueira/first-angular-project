7fcd9db3ce10f8fe7f240b02e044525e1dd3ee8c
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-09 14:16:05 -0300

    feat: create first properties

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type ICar = {
+  modelName: string;
+  year: number;
+};
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+  title = 'First Component title'; // by inference
+  number: number = 1; // explicit typed
+  car: ICar = {
+    modelName: 'Ford',
+    year: 2020,
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
